d1bdbda7a3c34919fe6c2f9cb103dd820ff08d8d
DSoloha <dsoloha@live.com>
2021-09-21 17:27:04 -0400

    Updated version, removed unneeded pronouns call

--- src/stores/character.store.ts
+++ src/stores/character.store.ts
@@ -1,6 +1,6 @@
 import { writable, Writable } from 'svelte/store';
 import { Character } from 'character-maker';
 
-const character: Writable<Character> = writable(new Character);
+const character: Writable<Character> = writable(new Character());
 
 export default character;
