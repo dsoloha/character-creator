ddd9a94b7db105725f57ebc566be013b4511b410
Dan <38593903+dsoloha@users.noreply.github.com>
2022-05-15 00:35:52 -0400

    Split Overview into subfiles

--- src/routes/overview.svelte
+++ src/routes/overview.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
   import character from '../stores/character.store'
   import { getPronouns } from '../classes/Pronouns'
-
-  $: He = getPronouns($character).He
+  import Eyes from './overview/eyes.svelte'
+  import Age from './overview/age.svelte'
+  import Sex from './overview/sex.svelte'
 </script>
 
 <main>
@@ -10,30 +11,9 @@
 
   {#if $character.name.first}
     <p>
-      <b>{$character.name.first}</b> is a <b>{$character.sex.type}</b>.
-      {#if $character.eyes.colors.left || $character.eyes.colors.right}
-        {#if $character.eyes.colors.left === $character.eyes.colors.right}
-          {He} has <b>{$character.eyes.colors.left}</b> eyes.
-        {:else}
-          {He} has a
-          {#if $character.eyes.colors.left}
-            <b>{$character.eyes.colors.left}</b> left eye{!$character.eyes.colors.right ? '.' : ''}
-          {/if}
-          {#if $character.eyes.colors.left && $character.eyes.colors.right}
-            and a
-          {/if}
-          {#if $character.eyes.colors.right}
-            <b>{$character.eyes.colors.right}</b> right eye.
-          {/if}
-        {/if}
-      {/if}
-      {#if $character.age.birth}
-        {He} was born on
-        <b
-          >{$character.age.birth.month}
-          {$character.age.birth.day}, {$character.age.birth.year}</b
-        >.
-      {/if}
+      <Sex />
+      <Eyes />
+      <Age />
     </p>
   {/if}
 </main>
---
+++ src/routes/overview/sex.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import character from '../../stores/character.store'
+</script>
+
+<b>{$character.name.first}</b> is a <b>{$character.sex.type}</b>.
